0c89ef56b431397ec0d5091d4c21dd2c1c791ead
Sai Balaji K <ksbalaji2000@outlook.com>
2026-08-06 21:03:11 +0530

    Initial Commit

---
+++ TamilInternetRadio/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TamilInternetRadio
+//
+//  Created by Sai Balaji on 06/08/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TamilInternetRadio/TamilInternetRadioApp.swift
@@ -0,0 +1,17 @@
+//
+//  TamilInternetRadioApp.swift
+//  TamilInternetRadio
+//
+//  Created by Sai Balaji on 06/08/26.
+//
+
+import SwiftUI
+
+@main
+struct TamilInternetRadioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
